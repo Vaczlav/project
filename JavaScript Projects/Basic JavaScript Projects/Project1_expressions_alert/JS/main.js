0fea4914d2c62ddc01daabe8e1702f605c5712a4
Vaczlav <106527580+Vaczlav@users.noreply.github.com>
2025-02-14 15:13:55 -0500

    add expression

--- JavaScript Projects/Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ JavaScript Projects/Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -10,3 +10,10 @@ window.alert(fullSentence);
 
 // Use document.write() to display the concatenated string on the webpage
 document.write(fullSentence);
+
+// Write an expression to calculate a sum, for example
+let sum = 5 + 10; // This is the expression
+window.alert("The sum of 5 and 10 is: " + sum); // Displaying the result of the expression
+
+// Writing the expression result to the document
+document.write("<br>The sum of 5 and 10 is: " + sum);
