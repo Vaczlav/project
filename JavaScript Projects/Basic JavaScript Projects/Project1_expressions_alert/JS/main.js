c496d1e17f2880a19bdd80107a6bf334d3f9500d
Vaczlav <106527580+Vaczlav@users.noreply.github.com>
2025-02-14 15:07:38 -0500

    Basic JavaScript Projects Assignment

---
+++ JavaScript Projects/Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,12 @@
+// Declare two variables with string values
+let Sent1 = "This is the beginning of the string";
+let Sent2 = " and this is the end of the string";
+
+// Concatenate the two string variables and store the result in a new variable
+let fullSentence = Sent1 + Sent2;
+
+// Use window.alert() to display the concatenated string in an alert box
+window.alert(fullSentence);
+
+// Use document.write() to display the concatenated string on the webpage
+document.write(fullSentence);
